b83c3c9bb986716e7631d34b3be13cc870cce1be
afra amini <afra.amini@gmail.com>
2020-11-20 14:40:11 +0100

    Reduce number of simulation tests

--- bleu-estimation.sh
+++ bleu-estimation.sh
@@ -1,6 +1,6 @@
 for i in `seq 2 49`;
         do
-        for j in `seq 1 50`;
+        for j in `seq 1 10`;
                 do
                   python generate.py --stochastic-beam-search --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
                 done
