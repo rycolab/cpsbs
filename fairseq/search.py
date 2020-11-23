954dbf667c9b96b109ae037cea636bdb80c44979
afra amini <afra.amini@gmail.com>
2020-11-23 16:06:36 +0100

    [DEBUG] Add print

--- fairseq/search.py
+++ fairseq/search.py
@@ -115,7 +115,7 @@ class CPS(Search):
 
     def cps_sample(self, p, k, bsz):
         self.p = p.detach().numpy()
-        self.p = np.concatenate((np.zeros((bsz, 1)), self.p), axis=1, dtype=float)
+        self.p = np.concatenate((np.zeros((bsz, 1)), self.p), axis=1)
         n = self.p.size()[1] - 1
         k = min(n, k)
 
