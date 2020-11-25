b841e34ff19e46bbfbc783ebcb1cba3d79b024b2
afra amini <afra.amini@gmail.com>
2020-11-25 14:28:25 +0100

    [DEBUG] Add prints

--- fairseq/search.py
+++ fairseq/search.py
@@ -115,6 +115,7 @@ class CPS(Search):
 
     def cps_sample(self, p, k, bsz):
         self.p = p.detach().numpy()
+        print(self.p)
         self.p = np.concatenate((np.zeros((bsz, 1)), self.p), axis=1)
         print(self.p)
         print(self.p[0, :])
