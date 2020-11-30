ae7d9f73fde400bed6579bd3b4477b7ccb682222
afra amini <afra.amini@gmail.com>
2020-11-30 20:01:30 +0100

    Add np to setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ bleu = Extension(
     ],
     extra_compile_args=['-std=c++11'],
 )
-cps = [Extension('fairseq.cps_dp', ["fairseq/cps_dp.pyx"])]
+cps = [Extension('fairseq.cps_dp', ["fairseq/cps_dp.pyx"], include_dirs=[numpy.get_include()])]
 setup(
     ext_modules=cythonize(cps)
 )
