3782b57709c56d733b96c2836305cf01e9b217f4
afra amini <afra.amini@gmail.com>
2020-12-03 18:46:05 +0100

    Add bleu estimation script

--- bleu-estimation.sh
+++ bleu-estimation.sh
@@ -1,7 +1,7 @@
-for i in `seq 2 49`;
+for i in `seq 2 25`;
         do
         for j in `seq 1 10`;
                 do
-                  python generate.py --stochastic-beam-search --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
+                  python generate.py --cps --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
                 done
         done
