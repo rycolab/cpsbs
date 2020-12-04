9fee05a2fce9630214615225a4f9b4f2e5ed798b
afra amini <afra.amini@gmail.com>
2020-12-04 16:46:37 +0100

    Remove inner loop from estimation for quick test

--- bleu-estimation.sh
+++ bleu-estimation.sh
@@ -1,7 +1,4 @@
 for i in `seq 2 25`;
         do
-        for j in `seq 1 10`;
-                do
-                  python generate.py --cps --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
-                done
+          python generate.py --cps --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
         done
