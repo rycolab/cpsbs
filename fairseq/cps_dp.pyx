ddf672650b65fe9e0f0e9195e703372efa4096a8
afra amini <afra.amini@gmail.com>
2020-12-29 15:14:35 +0100

    [DEBUG] Add prints

--- fairseq/cps_dp.pyx
+++ fairseq/cps_dp.pyx
@@ -62,9 +62,8 @@ def calc_normalization(np.ndarray[DTYPE_t, ndim=1] logp_sliced, int k):
     cdef int i
 
     for r in range(1, k + 1):
+        print(r)
         for i in prange(1, n + 1, nogil=True):
-            print(r)
-            print(i)
             intermediate_res = subset_sum_product_probs[r - 1, i - 1] + logp_sliced[i - 1]
             subset_sum_product_probs[r, i] = log_add(subset_sum_product_probs[r, i - 1], intermediate_res)
     return subset_sum_product_probs
