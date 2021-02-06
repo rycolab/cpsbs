2267a04d9e24916c4adb0931985003e34ee341f7
Afra Amini <afra.amini@gmail.com>
2021-02-06 13:22:51 +0100

    Delete bleu-estimation.sh

--- bleu-estimation.sh
+++
@@ -1,4 +0,0 @@
-for i in `seq 2 25`;
-        do
-          python generate.py --cps --beam $i --nbest $i --no-early-stop --unnormalized --sampling-temperature 0.1 data-bin/test --path ../fairseq/data-bin/wmt14.en-fr.fconv-py/model.pt
-        done
